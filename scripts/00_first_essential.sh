44305afd84880cfd31bf54af46438858e65f5083
lcx2019 <48182434+lcx2019@users.noreply.github.com>
2024-12-26 12:47:00 +0530

    Update 00_first_essential.sh

--- scripts/00_first_essential.sh
+++ scripts/00_first_essential.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 ##########################
-#      A hmasa a ma mawh  ho         #
-#                                                           #
+# A hmasa a ma mawh  ho  #
+#                        #
 ##########################
 
 sudo pacman -S  git nano kate htop curl sudo neofetch python3 timeshift rsync nmap
