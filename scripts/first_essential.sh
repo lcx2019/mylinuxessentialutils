5c091e0275925a5a42fd6f228a15231514a738c1
lcx2019 <48182434+lcx2019@users.noreply.github.com>
2024-12-26 12:45:38 +0530

    Add files via upload

---
+++ scripts/first_essential.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+##########################
+#      A hmasa a ma mawh  ho         #
+#                                                           #
+##########################
+
+sudo pacman -S  git nano kate htop curl sudo neofetch python3 timeshift rsync nmap
