4408c6236c66e7514c91a4891d8637bb39f61854
lcx2019 <48182434+lcx2019@users.noreply.github.com>
2025-01-01 11:42:04 +0530

    Update 02_yay_installer.sh

--- scripts/02_yay_installer.sh
+++ scripts/02_yay_installer.sh
@@ -7,13 +7,13 @@ echo "
 ##                   -chhana 01-jan-2025      ##
 ##                                            ##
 ################################################
-"
+\n"
 
 echo "
 ################################################
 ##        yay  Installing...                  ##
 ################################################
-"
+\n"
 sudo pacman -S --needed base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
