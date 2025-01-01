4b939b79535946eca1bc6215ef22176f1130b5f6
lcx2019 <48182434+lcx2019@users.noreply.github.com>
2025-01-01 11:35:07 +0530

    Add files via upload

---
+++ kaGitDownload.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "
+################################################
+##                                                                                                              ##
+##        Ka github a mi download na.                                                    ##
+##                   -chhana 01-jan-2025                                                        ##
+##                                                                                                               ##
+################################################
+"
+
+git clone https://github.com/lcx2019/mylinuxessentialutils.git
