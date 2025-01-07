aaa0f7e91e5c8dc821e58b6ad7ff95a1a9a67841
lcx2019 <48182434+lcx2019@users.noreply.github.com>
2025-01-07 15:52:11 +0530

    update

--- scripts/00_first_essential.sh
+++ scripts/00_first_essential.sh
@@ -5,4 +5,4 @@
 #                        #
 ##########################
 
-sudo pacman -S  --noconfirm git nano kate dpkg htop curl sudo neofetch python3 timeshift rsync nmap ntfs-3g gnome-disk-utility gparted xorg-xhost putty ghostty
+sudo pacman -S  --noconfirm git nano kate dpkg htop curl sudo neofetch python3 timeshift rsync nmap ntfs-3g gnome-disk-utility gparted xorg-xhost putty ghostty bchunk
