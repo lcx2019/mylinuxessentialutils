a03b70510b80d2a0a066d6ccd53a2c5536b08377
lcx2019 <48182434+lcx2019@users.noreply.github.com>
2025-08-03 10:07:00 +0530

    add more apps 00_first_essential.sh
    pacman atrang ->  ncdu duf tldr bat

--- scripts/00_first_essential.sh
+++ scripts/00_first_essential.sh
@@ -5,4 +5,5 @@
 #                        #
 ##########################
 
-sudo pacman -S  --noconfirm git nano kate dpkg htop curl sudo neofetch python3 timeshift rsync nmap ntfs-3g gnome-disk-utility gparted xorg-xhost putty ghostty bchunk
+#sudo pacman -S  --noconfirm git nano kate dpkg htop curl sudo neofetch python3 timeshift rsync nmap ntfs-3g gnome-disk-utility gparted xorg-xhost putty ghostty bchunk
+sudo pacman -S  --noconfirm git nano kate dpkg htop curl sudo neofetch python3 timeshift rsync nmap ntfs-3g gnome-disk-utility gparted xorg-xhost putty bchunk ncdu duf tldr bat
